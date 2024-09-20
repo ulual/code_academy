acf57404a6a9d9a55121453d1ba1a7414de27d68
Victor Libunao <129720171+ulual@users.noreply.github.com>
2024-09-20 03:10:54 -0700

    New file function_arguments

---
+++ function_arguments.py
@@ -0,0 +1,6 @@
+def print_animals(animal1, animal2, *args, animal4, **kwargs):
+  print(animal1, animal2)
+  print(args)
+  print(animal4)
+  print(kwargs)
+print_animals('Snake', 'Fish', 'Guinea Pig', 'Owl', animal4='Cat', animal5='Dog, Pigeon')
