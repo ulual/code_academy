6601c17b09309400f93b41350d3c928b40af4493
ulual <ulualibunao@gmail.com>
2024-09-28 20:12:40 -0700

    nonlocal

---
+++ nonlocal.py
@@ -0,0 +1,12 @@
+def enclosing_function():
+  var = "value"
+
+  def nested_function():
+    nonlocal var
+    var = "new_value"
+
+  nested_function()
+  print(var)
+
+enclosing_function()
+print(var)
