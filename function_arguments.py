78c696537c47e593dd6e9a6c1c03e63cd8dc63f1
ulual <ulualibunao@gmail.com>
2024-09-28 20:03:12 -0700

    Update function_arguments.py

--- function_arguments.py
+++ function_arguments.py
@@ -3,4 +3,5 @@ def print_animals(animal1, animal2, *args, animal4, **kwargs):
   print(args)
   print(animal4)
   print(kwargs)
-print_animals('Snake', 'Fish', 'Guinea Pig', 'Owl', animal4='Cat', animal5='Dog, Pigeon')
+print_animals('Snake', 'Fish', 'Guinea Pig', 'Owl', animal4='Cat', animal5='Dog, Pigeons')
+    elif price_for_driver < cheapest_driver_price:
