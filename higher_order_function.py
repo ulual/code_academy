7d1ab8d1e2578c6f3d18ca813abfd145852f1233
Victor Libunao <ulualibunao@gmail.com>
2024-10-03 01:26:49 -0700

    update higher_order_function

---
+++ higher_order_function.py
@@ -0,0 +1,16 @@
+def total_bill(func, value):
+    total = func(value)
+    return total
+
+def add_tax(total):
+  tax = total * 0.06
+  new_total = total + tax
+  return new_total
+
+def add_tip(total):
+  tip = total * .2
+  new_total = total + tip
+  return new_total
+
+print(total_bill(add_tax, 100))
+print(total_bill(add_tip, 100))
